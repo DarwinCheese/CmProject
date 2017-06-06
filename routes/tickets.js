37f4b6c45565b5655c05dfbd8c1a2703ed3d6756
svber <srjberni@avans.nl>
2017-06-06 11:52:09 +0200

    shit

--- routes/tickets.js
+++ routes/tickets.js
@@ -3,11 +3,38 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var request = require('request');
+var token
+var Sequence = exports.Sequence || require('sequence').Sequence
+    , sequence = Sequence.create()
+    , err
+    ;
 
-router.get('*', function(request, response) {
- response.status(200);
- response.json({
- "description": "Recipes REST server API version 1 is no longer supported. Please use API version 2."
- });
-});
+sequence
+    .then(function(next){
+        var auth = {
+        url : 'https://autocollectapi.cmpayments.com/v1.0/token',
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/x-www-form-urlencoded',
+            'X-CM-MERCHANT':'AMR-745B5C98-648A-45A1-ACB4-BDAD2AB9E936'
+        },
+        body: 
+            'grant_type=password&username=Avans1ApiUser&password=59bf8b536a0802561c8be4e3fd1b300847f5549d190499670921a3e40467d707'
+        };
+
+    request(auth, function(err, res, data, next) {  
+        var result = JSON.parse(data);
+        token = result.access_token;
+        console.log("token van request");
+       
+    })
+      setTimeout(function () {
+        next(err, token);
+      }, 120);
+})
+.then(function (next, token) {
+      console.log("het token");
+
+      });
+
 module.exports = router;
