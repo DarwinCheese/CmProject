b6c00e7c27d5a01a661831fbc57bdeaebe6e16cf
BeeBeeEight <teun977@gmail.com>
2017-06-08 12:01:43 +0200

    Add files via upload
    Accesstoken

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
-var http = require("http");
-var options = {
-    hostname: 'autocollectapi.cmpayments.com',
-    port: 80,
-    path: '/v1.0/payment-plans',
+var request = require('request');
+var access_token;
+
+
+var auth = {
+    url : 'https://autocollectapi.cmpayments.com/v1.0/token',
     method: 'POST',
     headers: {
-        'Authorisation': 'bearer NpqrKL3FitxmbapiZ0tc64iCuGSVZm-6FDMR_ndFWcSAEd4y0lWOUiJeJ2t7VVFpj-5VSjonn58QV8zxVQIdgP6bkb8JChy5s9vc0unLZUpdEzXaywe9HUfA2YqNC1UwB-_1F_FSwLBD0zombkPIIEtKvy35YQMH_oxZXqBpdigkeT_lStd4HYzciffCToBVEoPYWfmrbnF-GTEnOWyi0RtGSpXBF28ppzpjR44pt6NgP6a28QsSsgHsMX-K-gkxB4xc7w1h8ubYkZ_szySx5DMdORnzVYUn9W_zilVYErU',
+        'Content-Type':'application/x-www-form-urlencoded',
+        'X-CM-MERCHANT':'AMR-745B5C98-648A-45A1-ACB4-BDAD2AB9E936'
+    },
+    body: 'grant_type=password&username=Avans1ApiUser&password=59bf8b536a0802561c8be4e3fd1b300847f5549d190499670921a3e40467d707'
+};
+
+var paymentplans = {
+    url: 'https://autocollectapi.cmpayments.com/v1.0/payment-plans',
+    method: 'GET',
+    headers: {
+        'Authorisation': access_token, 
         'Content-Type': 'application/json'
     }
 };
 
-var req = http.request(options, function(res) {
-    console.log('Status: ' + res.statusCode);
-    console.log('headers: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data', function (body) {
-        console.log('Body: ' + body);
-    });
+request(auth, function(err, res, body) {  
+    var json = JSON.parse(body);
+    //console.log(json);
+    access_token = json.access_token;
+    console.log(access_token);
 });
 
-req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-});
 
-req.write('{"period_type": "Manually","number_of_periods":1,"periods":[{"payment_method": "iDEAL","delivery_method": "Email","delivery_time": "12:00"}]}');
-req.end();
+request(paymentplans, function(err, res, body){
+    var json = JSON.parse(body);
+    console.log(json);
+});
